186e4287c31f46822d45f8187b3d5c4919cc9444
AnkitSheokeen <ankit2100as@gmail.com>
2024-08-04 23:26:57 +0530

    button

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -38,14 +38,11 @@ function Navbar() {
         <div>
           <img src={navphoto} alt="Logoipsum" />
         </div>
-        <a
-          className={`nav__hamburger ${navActive ? "active" : ""}`}
-          onClick={toggleNav}
-        >
+        <button className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav} >
           <span className="nav__hamburger__line"></span>
           <span className="nav__hamburger__line"></span>
           <span className="nav__hamburger__line"></span>
-        </a>
+        </button>
         <div className={`navbar--items ${navActive ? "active" : ""}`}>
           <ul>
             <li>
